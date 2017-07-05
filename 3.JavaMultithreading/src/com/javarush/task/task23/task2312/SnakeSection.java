28c88d2b00460f8ddf37c3d69328d8bd36da7199
ADolodarenko <adolodar@gmail.com>
2017-07-05 20:42:10 +0300

    The snake is done

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -17,22 +17,22 @@ public class SnakeSection {
     public int getY() {
         return y;
     }
-    
+
     @Override
-    public boolean equals(Object o)
-    {
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        
+
         SnakeSection that = (SnakeSection) o;
-        
+
         if (x != that.x) return false;
-        return y == that.y;
+        if (y != that.y) return false;
+
+        return true;
     }
-    
+
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         int result = x;
         result = 31 * result + y;
         return result;
