5d642b205d57b563c6673b89235292a35a63e5cc
ADolodarenko <adolodar@gmail.com>
2017-07-17 17:36:22 +0300

    Super task of the level 38 is up to 7th part

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,7 +1,13 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +21,25 @@ public class HHStrategy implements Strategy
 	@Override
 	public List<Vacancy> getVacancies(String searchString)
 	{
+		try
+		{
+			Document document = getDocument(searchString, 0);
+
+			Elements elements = document.select("vacancy-serp__vacancy");
+			if (elements != null && elements.size() > 0)
+				for (Element element : elements)
+				{
+
+				}
+		}
+		catch (IOException e)
+		{}
+
 		return new ArrayList<>();
+	}
+
+	protected Document getDocument(String searchString, int page) throws IOException
+	{
+		return Jsoup.connect("http://javarush.ru/testdata/big28data.html").userAgent("Mozilla/5.0").referrer("no-referrer-when-downgrade").get();
 	}
 }
