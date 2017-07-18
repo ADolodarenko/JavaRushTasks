08183d3e679281953e6ba89318c8df1833466efa
ADolodarenko <adolodar@gmail.com>
2017-07-18 17:24:30 +0300

    Super task of the level 38 is up to 14th part

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -3,6 +3,9 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -11,8 +14,8 @@ import java.util.List;
 public class HtmlView implements View
 {
 	private Controller controller;
-	private final String filePath = "./src/" + this.getClass().getPackage().getName().replace(".", "/")
-			+ "/vacancies.html";
+	private final String filePath = System.getProperty("user.dir").replace("\\", "/") + "/4.JavaCollections/src/" + this.getClass().getPackage().getName().replace(".", "/") + "/vacancies.html";
+	//private final String filePath = "./src/" + this.getClass().getPackage().getName().replace(".", "/") + "/vacancies.html";
 
 	@Override
 	public void update(List<Vacancy> vacancies)
@@ -43,6 +46,16 @@ public class HtmlView implements View
 		return null;
 	}
 
-	private void updateFile(String fileName)
-	{}
+	private void updateFile(String content)
+	{
+		try (FileWriter writer = new FileWriter(filePath))
+		{
+			writer.write(content);
+			writer.flush();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
