f5e3629e8b9998bbf1c6beb6a7c7d493c5066c4b
ADolodarenko <adolodar@gmail.com>
2017-07-24 02:00:28 +0300

    Level39, super task is on the 10th part...

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task26.task2613;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.*;
 
 public class CurrencyManipulator
 {
@@ -42,5 +43,57 @@ public class CurrencyManipulator
 	public boolean hasMoney()
 	{
 		return !denominations.isEmpty();
+	}
+
+	public boolean isAmountAvailable(int expectedAmount)
+	{
+		return expectedAmount <= getTotalAmount();
+	}
+
+	public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException
+	{
+		Map<Integer, Integer> amounts = new HashMap<>();
+
+		Map<Integer, Integer> denoms = new TreeMap<>(new Comparator<Integer>()
+		{
+			@Override
+			public int compare(Integer o1, Integer o2)
+			{
+				return o2.compareTo(o1);
+			}
+		});
+		denoms.putAll(denominations);
+
+		Iterator<Map.Entry<Integer, Integer>> iterator = denoms.entrySet().iterator();
+
+		while (expectedAmount > 0 && iterator.hasNext())
+		{
+			Map.Entry<Integer, Integer> entry = iterator.next();
+			Integer key = entry.getKey();
+			Integer value = entry.getValue();
+
+			int quantity = expectedAmount/key;
+
+			if (quantity > 0 && value >= quantity)
+			{
+				amounts.put(key, quantity);
+
+				expectedAmount %= key;
+			}
+		}
+
+		if (!amounts.isEmpty())
+		{
+			for (Map.Entry<Integer, Integer> entry : amounts.entrySet())
+			{
+				Integer key = entry.getKey();
+				Integer value = denominations.get(key) - entry.getValue();
+				denominations.put(key, value);
+			}
+		}
+		else
+			throw new NotEnoughMoneyException();
+
+		return amounts;
 	}
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,10 +1,74 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 class WithdrawCommand implements Command
 {
 	@Override
-	public void execute()
+	public void execute() throws InterruptOperationException
 	{
+		String currencyCode = ConsoleHelper.askCurrencyCode();
+		CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+
+		while (true)
+		{
+			ConsoleHelper.writeMessage("Enter the sum:");
+			boolean isCorrect = false;
+			int sum = 0;
+			try
+			{
+				sum = Integer.parseInt(ConsoleHelper.readString());
+
+				if (sum > 0)
+					isCorrect = true;
+			}
+			catch (NumberFormatException e)
+			{}
+
+			if (!isCorrect)
+			{
+				ConsoleHelper.writeMessage("Your sum isn't correct.");
+
+				continue;
+			}
+
+			if (!manipulator.isAmountAvailable(sum))
+				continue;
+
+			try
+			{
+				Map<Integer, Integer> denominations = manipulator.withdrawAmount(sum);
+				Map<Integer, Integer> sortedDenominations = new TreeMap<>(new Comparator<Integer>()
+				{
+					@Override
+					public int compare(Integer o1, Integer o2)
+					{
+						return o2.intValue() - o1.intValue();
+					}
+				});
+
+				sortedDenominations.putAll(denominations);
+
+				for (Map.Entry<Integer, Integer> entry : sortedDenominations.entrySet())
+					ConsoleHelper.writeMessage("\t" + entry.getKey() + " - " + entry.getValue());
+
+				ConsoleHelper.writeMessage("Transaction succeeded.");
+
+				break;
+			}
+			catch (NotEnoughMoneyException e)
+			{
+				ConsoleHelper.writeMessage("There aren't enough denominations.");
+			}
+		}
 
 	}
 }
